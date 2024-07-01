c399833d289b145262a4f57357617f1fbd45123c
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-01 09:20:27 -0500

    BooleanLogicAssignment
    Created a basic approval program for car insurance.

---
+++ BooleanLogicAssignment.cs/BooleanLogicAssignment.cs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            Console.WriteLine("" + yourAge);
+            Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string yourAnswer = Console.ReadLine();
+            Console.WriteLine("" + yourAnswer);
+            Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+            Console.WriteLine("" + yourTickets);
+            Console.ReadLine();
+
+            int num1 = 15;
+            int num2 = 3;
+
+            Console.WriteLine("Is this person qualified?");
+            Console.ReadLine();
+            bool isQualified = (yourAge >= num1 && yourAnswer == "No" && yourTickets < num2);
+            Console.WriteLine(isQualified);
+
+        }
+    }
+}
