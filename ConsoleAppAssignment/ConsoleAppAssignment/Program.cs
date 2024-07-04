9d56e20026e1cc1746ec1ef6aaca1f8d12e05ab4
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-04 09:33:30 -0500

    ConsoleAppAssignment
    Created a console app that does a boolean comparison using a do/while statement.

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 24;
+
+            do
+            {
+                switch (number)
+                {
+                    case 63:
+                        Console.WriteLine("You guessed 63. Please Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 27:
+                        Console.WriteLine("You guessed 27. Please Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 60:
+                        Console.WriteLine("You guessed 60. Please Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 24:
+                        Console.WriteLine("You guessed the number 24. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are Wrong.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
+
+            Console.Read();
+
+        }
+    }
+}
