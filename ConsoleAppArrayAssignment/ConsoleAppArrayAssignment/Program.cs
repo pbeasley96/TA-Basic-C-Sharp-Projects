a6b8bdfc608186a4aa6297b560f652d7166241bd
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-10 11:15:46 -0500

    ConsoleAppArrayAssignment
    Edits made for re submission.

--- ConsoleAppArrayAssignment/ConsoleAppArrayAssignment/Program.cs
+++ ConsoleAppArrayAssignment/ConsoleAppArrayAssignment/Program.cs
@@ -3,56 +3,27 @@ using System.Collections.Generic;
 
 
 class Program
- {
+{
     static void Main()
     {
         //Array of Integers
-        int[] numArray = new int[5];
-        numArray[0] = 2;
-        numArray[1] = 4;
-        numArray[2] = 8;
-        numArray[3] = 16;
-        numArray[4] = 32;
+        int[] numArray = new int[5] { 2, 4, 8, 16, 32 };
 
         Console.WriteLine("Please select an index of the Array and display the integer at that index on the screen.");
         int input = Convert.ToInt32(Console.ReadLine());
-
-        for (int i = 0; i < numArray.Length; i++)
-        {
-            if (input >= 0 && input < 4)
-            {
-                Console.WriteLine(numArray[i]);
-            }
-            else
-            {
-                Console.WriteLine("You have selected and index that doesn't exist. Please select again.");
-            }
-
-        }
+        int intOutput = numArray[input];
+        
+        Console.WriteLine(intOutput);
         Console.ReadLine();
 
         //Array of Strings
-        string[] strArray = new string[3] { "Element 1", "Element 2", "Element 3"};
+        string[] strArray = new string[3] { "Element 1", "Element 2", "Element 3" };
 
         Console.WriteLine("Please select an index of the Array and display the string at that index on the screen.");
-        string input2 = Console.ReadLine();
+        int input1 = Convert.ToInt32(Console.ReadLine());
+        string strOuput = strArray[input1];
 
-        if (input2.Contains(strArray[0]))
-        {
-            Console.WriteLine("");
-        }
-        else if (input2.Contains(strArray[1]))
-        {
-            Console.WriteLine("");
-        }
-        else if (input2.Contains(strArray[2]))
-        {
-            Console.WriteLine("");
-        }
-        else
-        {
-            Console.WriteLine("You have selected and index that doesn't exist. Please select again.");
-        }
+        Console.WriteLine(strOuput);
         Console.ReadLine();
 
         //List of Strings
@@ -63,13 +34,11 @@ class Program
         colorList.Add("black");
 
         Console.WriteLine("Please select an index of the list and display the content at that index on the screen.");
+        int input2 = Convert.ToInt32(Console.ReadLine());
+        string ListOutput = colorList[input2];
+
+        Console.WriteLine(ListOutput);
         Console.ReadLine();
 
-        foreach (string item in colorList)
-        {
-            if (item.Contains(LinkedList))
-                return item;
-        }
-        Console.ReadLine();
     }
 }
