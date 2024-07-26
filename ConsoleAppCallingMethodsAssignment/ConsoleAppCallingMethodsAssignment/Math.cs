aa5ae664eb6376a35397bd37f0b1a15a782e8755
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-26 08:28:29 -0500

    ConsoleAppCallingMethodsAssignment
    Create a console App.
    Create a class and within that class create three methods.

---
+++ ConsoleAppCallingMethodsAssignment/ConsoleAppCallingMethodsAssignment/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCallingMethodsAssignment
+{
+    public class Math //Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer.
+    {
+        public int Add { get; set; }
+        public int Multiply { get; set; }
+        public int Divide { get; set; }
+    }
+}
---
+++ ConsoleAppCallingMethodsAssignment/ConsoleAppCallingMethodsAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> mathNumber = new List<int>() { 10, 20, 30, 40, 50 };
+
+
+            Console.WriteLine("Please select a number you want to do Math Operations on."); //Ask the user what number they want to do the math operations on.
+
+            Math mathAdd = new Math();  //Call each method in turn, passing the user input to the method.
+            mathAdd.Add = Convert.ToInt32(mathNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Select a number to add."); //Ask the User what number they want to add the first number by.
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathAdd.Add + input);  //Display the returned integer to the screen. The process is repeated for the next inputs
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please select a number you want to do Math Operations on.");
+
+            Math mathMultiply = new Math();
+            mathMultiply.Multiply = Convert.ToInt32(mathNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Select a number to multiply.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathMultiply.Multiply + input1);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Please select a number you want to do Math Operations on.");
+
+            Math mathDivide = new Math();
+            mathDivide.Divide = Convert.ToInt32(mathNumber);
+            Console.ReadLine();
+
+            Console.WriteLine("Select a number to divide by.");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathDivide.Divide + input2);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
