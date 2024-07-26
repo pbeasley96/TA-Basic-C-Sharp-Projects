8f05823a6fc5f7e24c7c042bf385b6feb12b72e6
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-26 10:29:45 -0500

    ConsoleAppMainMethodAssignment
    Create a class. In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer.

---
+++ ConsoleAppMainMethodAssignment/ConsoleAppMainMethodAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer intAdd = new Integer();
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(intAdd + input);
+            Console.ReadLine();
+        }
+    }
+}
