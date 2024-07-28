137adc5c24e3a28bfec92347c429ee17659e4860
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-28 09:55:35 -0500

    ConsoleAppClassAssignment
    Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.

---
+++ ConsoleAppClassAssignment/ConsoleAppClassAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassAssignment
+{
+    public class Math 
+    {
+        public int Divide(int number)
+        {
+            number = 50;
+            int value = number / 2;
+            return value;
+        }
+        public void PrintedEquation(int value)
+        {
+            Console.WriteLine("{0}", value);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathDivide = new Math();
+            ex.Divide();
+            ex.PrintedEquation();
+        }
+    }
+}
