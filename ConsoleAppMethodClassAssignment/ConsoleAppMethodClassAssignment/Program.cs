bf4b04a10b6c4209de24b521d4fb61a90283950a
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-28 06:08:54 -0500

    ConsoleAppMethodAssignment
    Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.

---
+++ ConsoleAppMethodClassAssignment/ConsoleAppMethodClassAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethodClassAssignment
+{
+    class Math
+    {
+        public int mathVal(int number, int number2)
+        {
+            int value1 = number + 20;
+            int value2 = number2;
+            return value1;
+            return value2;
+
+        }
+        public static void Main()
+        {
+            Console.WriteLine();
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
