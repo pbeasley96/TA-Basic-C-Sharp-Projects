410f5f4a8f0799ff56b5cdf0e3f06df4a22345b1
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-29 00:31:06 -0500

    ConsoleAppMethodandObjectsAssignment
    Create a Console App that creates a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.

---
+++ ConsoleAppMethodsandObjectsAssignment/ConsoleAppMethodsandObjectsAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppMethodsandObjectsAssignment
+{
+    public class Person //Create a class called Person and give it two properties.
+    {
+        public string FirstName(string name)
+        {
+            string valueFirst = "Sample";
+            return valueFirst;
+        }
+        public string LastName(string name)
+        {
+            string valueLast = "Student";
+            return valueLast;
+        }
+
+        public void SayName() //A void method called SayName() that takes no parameters and simply writes the person's full name to the console.
+        {
+            Console.WriteLine("Name: [full name]");
+        }
+    }
+
+    public class Employee  //Create another class called Employee and have it inherit from the Person class.
+    {
+        public int PropertyID(int number) // Give the Employee class a property called Id and have it be of data type int.
+        {
+            int valueID = number;
+            return valueID;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person employeeName = new Person(); //Instantiate and initialize an Employee object with a first name of “Sample” and a last name of “Student”.
+            employeeName.SayName(); //Call the superclass method SayName() on the Employee object.
+            Console.WriteLine(employeeName.FirstName + "" + employeeName.LastName);
+        }
+    }
+}
