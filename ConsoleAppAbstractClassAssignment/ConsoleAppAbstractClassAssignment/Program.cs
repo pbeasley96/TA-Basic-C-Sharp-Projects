91d95dd82494b78a5de02d53b282f9aeb7e91295
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-30 09:23:26 -0500

    ConsoleAppAbstractClassAssignment

--- ConsoleAppAbstractClassAssignment/ConsoleAppAbstractClassAssignment/Program.cs
+++ ConsoleAppAbstractClassAssignment/ConsoleAppAbstractClassAssignment/Program.cs
@@ -8,18 +8,18 @@ namespace ConsoleAppAbstractClassAssignment
 {
     abstract class Person //Create an abstract class called Person with two properties: string firstName and string lastName.
     {
-        public abstract void EmpName(string fName, string lName);
+        public abstract string FName { get; set; }
+        public abstract string LName { get; set; }
+        public abstract void SayName();
+    }
 
-        public void SayName() //Give it the method SayName().
+    class Employee : Person //Create another class called Employee and have it inherit from the Person class.
+    {
+        public override string FName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string LName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override void SayName() //Implement the SayName() method inside of the Employee class.
         {
-            Console.WriteLine("Name: [full name]");
-        }
-        public class Employee : Person //Create another class called Employee and have it inherit from the Person class.
-        {
-            public override void EmpName(string fName, string lName)
-            {
-                throw new NotImplementedException();
-            }
+            Console.WriteLine("Name: ");
         }
     }
 
@@ -29,13 +29,13 @@ namespace ConsoleAppAbstractClassAssignment
     {
         static void Main(string[] args)
         {
-            Person employeeName = new Person(); //Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. 
+            Employee employeeName = new Employee(); //Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. 
             string input = "Sample";
             string input2 = "Student";
-            string resultofAbstract = Convert.ToString(input + input2);
+            string resultofAbstract = Convert.ToString(input + " " + input2);
             employeeName.SayName(); //Call the SayName() method on the object.
             Console.WriteLine(resultofAbstract);
-
+            Console.ReadLine();
         }
     }
 }
