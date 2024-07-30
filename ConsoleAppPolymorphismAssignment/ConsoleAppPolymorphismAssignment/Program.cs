e0a44f732de17244b044bd31ae8022314189eee0
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-30 08:52:37 -0500

    ConsoleAppPolymorphism
    Create a console app that create an interface called IQuittable and have it define a void method called Quit().

---
+++ ConsoleAppPolymorphismAssignment/ConsoleAppPolymorphismAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPolymorphismAssignment
+{
+    interface IQuittable //Create an interface called IQuittable and have it define a void method called Quit().
+    {
+        void IQuit();
+    }
+
+    class Employee : IQuittable //Employee Class will inherit that interface and implement the Quit() method.
+    {
+        public void IQuit()
+        {
+            Console.WriteLine("The Employee says: I Quit!");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee myEmployee = new Employee(); //Use polymorphism to create an object of type IQuittable and call the Quit() method on it.
+            myEmployee.IQuit();
+            Console.ReadLine();
+        }
+    }
+}
