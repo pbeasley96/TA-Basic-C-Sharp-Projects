3ac3bdfdfaca5ed7c9a8654f710b101b893bb502
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-30 10:38:40 -0500

    ConsoleAppMethodAssignment

--- ConsoleAppMethodClassAssignment/ConsoleAppMethodClassAssignment/Program.cs
+++ ConsoleAppMethodClassAssignment/ConsoleAppMethodClassAssignment/Program.cs
@@ -8,15 +8,10 @@ namespace ConsoleAppMethodClassAssignment
 {
     class Math //Create a class.
     {
-        public void MathVal(int value, int value2, out int result) //In that class, create a void method that takes two integers as parameters.
+        public void MathVal(int value, int value2) //In that class, create a void method that takes two integers as parameters.
         {
-            result = value + value2;
-        }
-        public void Display() //Have the method do a math operation on the first integer and display the second integer to the screen. 
-        {
-            int result;
-            MathVal(50, 50, out result);
-            Console.WriteLine("Result:" + result);
+            int result = value + 50;
+            Console.WriteLine(value2);
         }
 
         internal int MathVal()
@@ -33,6 +28,19 @@ namespace ConsoleAppMethodClassAssignment
             Math mathVal = new Math(); //In the Main() method of the console app, instantiate the class.
             int resultofMethod = mathVal.MathVal();
             Console.WriteLine(resultofMethod);
+            Console.ReadLine();
+
+            Math mathParameters = new Math(); //Call the method in the class, passing in two numbers.
+            int input1 = 50;
+            int input2 = 50;
+            int resultOFMethod = mathParameters.MathVal(input1, input2);
+            Console.WriteLine(resultOFMethod);
+            Console.ReadLine();
+
+            Math mathPass = new Math(); //Call the method in the class, specifying the parameters by name.
+            int methodPass = mathPass.MathVal(value, value2);
+            Console.WriteLine(methodPass);
+            Console.ReadLine();
         }
     }
 }
