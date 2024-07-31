a98df8160f201bc51421aef91ffd5fb07386e191
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-07-31 07:46:38 -0500

    ConsoleAppAbstractClassAssignment

--- ConsoleAppAbstractClassAssignment/ConsoleAppAbstractClassAssignment/Program.cs
+++ ConsoleAppAbstractClassAssignment/ConsoleAppAbstractClassAssignment/Program.cs
@@ -8,18 +8,16 @@ namespace ConsoleAppAbstractClassAssignment
 {
     abstract class Person //Create an abstract class called Person with two properties: string firstName and string lastName.
     {
-        public abstract string FName { get; set; }
-        public abstract string LName { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
         public abstract void SayName();
     }
 
     class Employee : Person //Create another class called Employee and have it inherit from the Person class.
     {
-        public override string FName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string LName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public override void SayName() //Implement the SayName() method inside of the Employee class.
         {
-            Console.WriteLine("Name: ");
+            Console.WriteLine($"Name: {FName} {LName} ");
         }
     }
 
@@ -29,12 +27,8 @@ namespace ConsoleAppAbstractClassAssignment
     {
         static void Main(string[] args)
         {
-            Employee employeeName = new Employee(); //Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. 
-            string input = "Sample";
-            string input2 = "Student";
-            string resultofAbstract = Convert.ToString(input + " " + input2);
+            Employee employeeName = new Employee { FName = "Sample", LName = "Student"}; //Inside the Main() method, instantiate an Employee object with firstName “Sample” and lastName “Student”. 
             employeeName.SayName(); //Call the SayName() method on the object.
-            Console.WriteLine(resultofAbstract);
             Console.ReadLine();
         }
     }
