a6b2cfc582045cdfa01f9584427061b6e685e7f9
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-08-02 09:11:17 -0500

    ConsoleAppOperatorAssignment
    Create a console app that creates an Employee class. In that class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.

---
+++ ConsoleAppOperatorAssignment/ConsoleAppOperatorAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOperatorAssignment
+{
+    class Program
+    {
+        public class Employee //Create an Employee class with Id, FirstName and LastName properties. 
+        {
+            private string firstname;
+            public string FirstName
+            {
+                get { return firstname; }
+                set { firstname = value; }
+            }
+            private string lastname;
+            public string LastName
+            {
+                get { return lastname; }
+                set { lastname = value; }
+            }
+            private string id;
+            public string ID
+            {
+                get { return id; }
+                set { id = value; }
+            }
+            public static bool operator == () 
+            {
+              
+            }
+            public static bool operator !=()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
