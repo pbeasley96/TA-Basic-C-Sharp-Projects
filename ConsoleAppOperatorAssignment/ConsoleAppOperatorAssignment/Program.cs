e02063c7bef80a7bb553566a2f9d57248ddaeaa8
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-08-03 09:17:31 -0500

    ConsoleAppOperatorAssignment

--- ConsoleAppOperatorAssignment/ConsoleAppOperatorAssignment/Program.cs
+++ ConsoleAppOperatorAssignment/ConsoleAppOperatorAssignment/Program.cs
@@ -28,18 +28,31 @@ namespace ConsoleAppOperatorAssignment
                 get { return id; }
                 set { id = value; }
             }
-            public static bool operator == () 
+            public static bool operator ==(Employee e1, Employee e2) //In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property.
             {
-              
+                if (e1 is null)
+                    return e2 is null;
+                return e1.Equals(e2);
             }
-            public static bool operator !=()
+            public static bool operator !=(Employee e1, Employee e2)
             {
-
+                return !(e1 == e2);
+            }
+            public override bool Equals(object obj)
+            {
+                if (obj == null)
+                    return false;
+                return obj is Employee e2 ? (firstname == e2.firstname &&
+                                            lastname == e2.lastname &&
+                                            id == e2.id) : false;
             }
         }
 
         static void Main(string[] args)
         {
+            Employee employeeID = new Employee(); //Instantiate two objects of the Employee class and assign values to their properties. 
+
+
         }
     }
 }
