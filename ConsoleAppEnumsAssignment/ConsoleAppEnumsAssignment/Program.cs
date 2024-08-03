0612d6b1e031bb73584634e4d8b56a1010414436
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-08-03 10:07:47 -0500

    ConsoleAppEnumsAssignment
    Create a console app that contains an enum for the days of the week.

---
+++ ConsoleAppEnumsAssignment/ConsoleAppEnumsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEnumsAssignment
+{
+    public class Week {
+        enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }; //Create an enum for the days of the week.
+
+        public static void Main()
+        {
+            Console.WriteLine("Please select a day in the week."); //Prompt the user to enter the current day of the week.
+            foreach (string weekDay in Enum.GetNames(typeof(Days)))
+            {
+                Console.WriteLine("You've selected ", weekDay, Enum.Parse(typeof(Days)));
+            }
+        catch (Exception) //Wrap the above statement in a try/catch block to the console if an error occurs.
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
