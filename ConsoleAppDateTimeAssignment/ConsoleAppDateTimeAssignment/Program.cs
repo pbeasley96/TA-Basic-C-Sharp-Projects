4193922b53ed0fc6e3f6c9dd0a4acd9f9bf1ded7
pbeasley96 <156345819+pbeasley96@users.noreply.github.com>
2024-08-13 08:05:06 -0500

    ConsoleAppDateTimeAssignment
    Create a console app that prints the current date and time to the console.

---
+++ ConsoleAppDateTimeAssignment/ConsoleAppDateTimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDateTimeAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeoftoday = new DateTime(2024, 8, 13, 7, 45, 00); //Prints the current date and time to the console.
+            Console.WriteLine(timeoftoday.ToString());
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a number."); //Asks the user for a number.
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(timeoftoday.AddHours(input)); //Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+            Console.ReadLine();
+
+        }
+    }
+}
